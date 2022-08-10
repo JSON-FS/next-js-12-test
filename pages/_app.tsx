5ff5257c4fe1bd31f1f4a714c163efa8e536ff02
JSON-G <gjasonaaron@gmail.com>
2022-08-10 15:39:15 +0800

    test simple routing static/ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Link href="/">Home</Link>
+      <br></br>
+      <Link href="/about">About</Link>
+      <br></br>
+      <Link href="/redir">Redit</Link>
+      <hr></hr>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
